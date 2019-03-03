1edddb25865d66dc2eaae1498cd01b3f7b74dd18
Adrian Conlon <Adrian.conlon@gmail.com>
2019-03-03 10:47:36 +0000

    Add a skeleton InvadersNet project, with EightBitNet as a dependency.
    Signed-off-by: Adrian Conlon <Adrian.conlon@gmail.com>

---
+++ InvadersNet/Program.cs
@@ -0,0 +1,15 @@
+// <copyright file="Program.cs" company="Adrian Conlon">
+// Copyright (c) Adrian Conlon. All rights reserved.
+// </copyright>
+
+namespace InvadersNet
+{
+    using System;
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
