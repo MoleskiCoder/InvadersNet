4c432fed0a89c006001ee874f67bfb91a9b91b11
Adrian Conlon <Adrian.conlon@gmail.com>
2019-03-20 22:48:58 +0000

    Refactor the render code to make the gel layout easier to understand.
    Signed-off-by: Adrian Conlon <Adrian.conlon@gmail.com>

--- InvadersNet/Cabinet.cs
+++ InvadersNet/Cabinet.cs
@@ -17,13 +17,13 @@ namespace Invaders
         private readonly GraphicsDeviceManager graphics;
         private readonly ColourPalette palette = new ColourPalette();
         private readonly Color[] pixels = new Color[DisplayWidth * DisplayHeight];
+        private readonly Color[] gel = new Color[DisplayWidth * DisplayHeight];
         private readonly List<Keys> pressed = new List<Keys>();
         private readonly Vector2 texturePosition = new Vector2(0, DisplayWidth * PixelSize);
         private readonly Vector2 textureOrigin = new Vector2(0, 0);
         private readonly float textureScale = PixelSize;
         private readonly SpriteEffects flipped = SpriteEffects.FlipHorizontally | SpriteEffects.FlipVertically;
         private readonly SpriteEffects unflipped = SpriteEffects.None;
-
         private SpriteBatch spriteBatch;
         private Texture2D bitmapTexture;
 
@@ -64,8 +64,9 @@ namespace Invaders
         {
             this.spriteBatch = new SpriteBatch(this.GraphicsDevice);
             this.palette.Load();
-            this.ChangeResolution(DisplayHeight, DisplayWidth); // Note the reversed layout: 90 degree rotation of display
+            this.ChangeResolution(DisplayHeight, DisplayWidth); // Note the reversed layout: -90 degree rotation of display
             this.bitmapTexture = new Texture2D(this.GraphicsDevice, DisplayWidth, DisplayHeight);
+            this.CreateGelPixels();
             this.Motherboard.Initialize();
             this.Motherboard.RaisePOWER();
         }
@@ -170,40 +171,36 @@ namespace Invaders
 
         private int DrawFrame(int prior)
         {
-            this.spriteBatch.Begin();
-            try
+            for (var y = 0; y < DisplayHeight; ++y)
             {
-                for (var y = 0; y < DisplayHeight; ++y)
+                if (y == 96)
                 {
-                    if (y == 96)
-                    {
-                        this.Motherboard.TriggerInterruptScanLine96();
-                    }
-
-                    prior = this.Motherboard.RunScanLine(prior);
-
-                    this.DrawScanLine(y);
+                    this.Motherboard.TriggerInterruptScanLine96();
                 }
 
-                this.Motherboard.TriggerInterruptScanLine224();
-
-                this.bitmapTexture.SetData(this.pixels);
-
-                var flip = Configuration.CocktailTable && this.Motherboard.CocktailModeControl;
-                var effect = flip ? this.flipped : this.unflipped;
-                this.spriteBatch.Draw(this.bitmapTexture, this.texturePosition, null, Color.White, ScreenRotation, this.textureOrigin, this.textureScale, effect, 0);
-            }
-            finally
-            {
-                this.spriteBatch.End();
+                prior = this.Motherboard.RunScanLine(prior);
+                this.PaintScanLine(y);
             }
 
+            this.bitmapTexture.SetData(this.pixels);
+            this.DrawTexture();
+
+            this.Motherboard.TriggerInterruptScanLine224();
             return this.Motherboard.RunVerticalBlank(prior);
         }
 
-        private void DrawScanLine(int y)
+        private void DrawTexture()
         {
-            var blackColour = this.palette.Colour(ColourPalette.ColourIndex.Black);
+            var flip = Configuration.CocktailTable && this.Motherboard.CocktailModeControl;
+            var effect = flip ? this.flipped : this.unflipped;
+
+            this.spriteBatch.Begin();
+            this.spriteBatch.Draw(this.bitmapTexture, this.texturePosition, null, Color.White * 0.5f, ScreenRotation, this.textureOrigin, this.textureScale, effect, 1);
+            this.spriteBatch.End();
+        }
+
+        private void PaintScanLine(int y)
+        {
             var bytesPerScanLine = DisplayWidth >> 3;
             var address = (ushort)(y * bytesPerScanLine);
             for (var byteX = 0; byteX < bytesPerScanLine; ++byteX)
@@ -214,9 +211,20 @@ namespace Invaders
                     var x = (byteX << 3) + bit;
                     var mask = 1 << bit;
                     var pixel = video & mask;
+                    var index = x + (y * DisplayWidth);
+                    this.pixels[index] = pixel == 0 ? Color.Black : this.gel[index];
+                }
+            }
+        }
+
+        private void CreateGelPixels()
+        {
+            for (var y = 0; y < DisplayHeight; ++y)
+            {
+                for (var x = 0; x < DisplayWidth; ++x)
+                {
                     var colourIndex = (int)ColourPalette.CalculateColour(y, DisplayWidth - x - 1);
-                    var colour = pixel == 0 ? blackColour : this.palette.Colour(colourIndex);
-                    this.pixels[x + (y * DisplayWidth)] = colour;
+                    this.gel[x + (y * DisplayWidth)] = this.palette.Colour(colourIndex);
                 }
             }
         }
